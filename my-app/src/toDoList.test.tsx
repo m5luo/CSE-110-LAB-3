23faf5deb70cbfc60fef9bbe94a13e81bbe22859
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-10-17 21:39:09 -0700

    Pushed todo list edge case

--- my-app/src/toDoList.test.tsx
+++ my-app/src/toDoList.test.tsx
@@ -15,13 +15,32 @@ describe("Create ToDoList", () => {
 });
 
 
+describe("Check Checkbox Increment", () => {
+    test('checkbox item bought counter increments after checking', ()=>{
+        render(<ToDoList />);
+        const checkbox = screen.getAllByTestId('checkboxID');
+        const counter = screen.getByText('Items bought: 0');
 
-test('checkbox item brought increments counter', ()=>{
-    render(<ToDoList />);
-    const checkbox = screen.getAllByTestId('checkboxID');
-    const counter = screen.getByText('Items bought: 0');
+        fireEvent.click(checkbox[0]);
 
-    fireEvent.click(checkbox[0]);
+        expect(counter).toHaveTextContent('Items bought: 1');
+    })
+});
 
-    expect(counter).toHaveTextContent('Items bought: 1');
+
+describe("Check Checkbox Increment and Decrement", () => {
+    test('checkbox item bought counter increments and decrements after checking and unchecking', ()=>{
+        render(<ToDoList />);
+        const checkbox = screen.getAllByTestId('checkboxID');
+        const counter = screen.getByText('Items bought: 0');
+        expect(counter).toHaveTextContent('Items bought: 0');
+        
+        fireEvent.click(checkbox[1]);
+
+        expect(counter).toHaveTextContent('Items bought: 1');
+
+        fireEvent.click(checkbox[0]);
+        
+        expect(counter).toHaveTextContent('Items bought: 0');
+    })
 });
